36256fdf59c1c18325d1f5e4e76fd879b5bda2ca
khadim <khadimkc@gmx.com>
2017-08-11 16:39:11 +0200

    Classe TestAnalyzer avec un type String en argument

---
+++ Java_files/src/Test.java
@@ -0,0 +1,8 @@
+
+public class Test {
+	public static void main(String[] args) {
+		String text="The quick brown fox jumped over the lazy brown dog’s back";
+		String[] tab = text.split(" ");
+		System.out.println(tab[1]);
+	}
+}
---
+++ Java_files/src/TextAnalyzer.java
@@ -0,0 +1,15 @@
+public class TextAnalyzer {
+
+	String text;
+	
+	public TextAnalyzer(String text) {
+		this.text=text;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
