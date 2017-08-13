28d63b3dd9efbe4df20d8dba8bf524afd1879bad
khadim <khadimkc@gmx.com>
2017-08-13 14:42:19 +0200

    Some improvements: replace method pushes now, some tests

--- Java_files/src/Test.java
+++ Java_files/src/Test.java
@@ -2,11 +2,24 @@
 public class Test {
 	public static void main(String[] args) {
 		String text="The quick brown fox jumped over the lazy brown dog’s back";
-		TextAnalyzer textToAnalyze= new TextAnalyzer(text);
+		String text2="Java is a general-purpose computer programming language that "
+				+ "is concurrent, class-based, object-oriented,[14] and specifically"
+				+ " designed to have as few implementation dependencies as possible. "
+				+ "It is intended to let application developers "
+				+ "\"write once, run anywhere\" (WORA),[15] meaning that compiled "
+				+ "Java code can run on all platforms that support Java without the"
+				+ " need for recompilation.[16] Java applications are typically compiled "
+				+ "to bytecode that can run on any Java virtual machine (JVM) regardless"
+				+ " of computer architecture. As of 2016, Java is one of the most popular"
+				+ " programming languages in use,[17][18][19][20] particularly for client-server"
+				+ " web applications, with a reported 9 million developers.[21] Java was"
+				+ " originally developed by James Gosling at Sun Microsystems (which has since "
+				+ "been acquired by Oracle Corporation) and released in 1995 as a core component"
+				+ " of Sun Microsystems' Java platform. The language derives much of its "
+				+ "syntax from C and C++, but it has fewer low-level facilities than either "
+				+ "of them.";
+		TextAnalyzer textToAnalyze= new TextAnalyzer(text2);
 				
-		String[] countedTable= textToAnalyze.count();
-		for( int i=0; i<countedTable.length; i++) {
-			System.out.println(countedTable[i]);
-		}
+		textToAnalyze.analyze();
 	}
 }
--- Java_files/src/TextAnalyzer.java
+++ Java_files/src/TextAnalyzer.java
@@ -1,40 +1,57 @@
 import java.util.Arrays;
 
+/**
+ * @author Cheikh KASSE
+ *
+ */
 public class TextAnalyzer {
 
-	String text;
+	private String text;
 	
+	/**
+	 * @param text
+	 * Constructor: Construct an object containing the text to analyze
+	 */
 	public TextAnalyzer(String text) {
 		this.text=text;
 	}
 		
+	/**
+	 * @return
+	 * Getter
+	 */
 	public String getText() {
-	//Getter
 		return text;
 	}
 
 
+	/**
+	 * @param text
+	 * Setter
+	 */
 	public void setText(String text) {
-	//Setter
 		this.text = text;
 	}
 
 
+	/**
+	 * @return
+	 * This method converts the text to analyze into a 
+	 * table using the blank spaces as separators 
+	 */
 	private String[] cut() {
-	//This method converts the text to analyze into a table using the blank spaces as separators 
-		String[] tab= text.split(" ") ;
+		String[] tab= this.getText().split(" ") ;
 		return tab;
 	}
 	
-	private static void replace(String[] table,int i, int j) {
-	//Exchange values of two given indices of a table 
-		String temp=table[i];
-		table[i]=table[j];
-		table[j]=temp;
-	}
-	
+	/**
+	 * @param table
+	 * @param start
+	 * @param end
+	 * @return
+	 * Extract a sub-table from a table between two given indices 
+	 */
 	private static String[] subTable(String[] table,int start, int end){
-	//Extract a sub-table from a table between two given indices 
 		String[] subTable = new String[end-start+1];
 		for(int index=start; index<=end; index++) {
 			subTable[index-start]=table[index];
@@ -42,8 +59,30 @@ public class TextAnalyzer {
 		return subTable;
 	}
 	
+	/**
+	 * @param table
+	 * @param i
+	 * @param j
+	 * Puts an index of a table in another place and pushes all the elements left	 */
+	private static void replace(String[] table,int i, int j) {
+		String temp = table[i];
+		table[i]=table[j];
+		String[] subtable = subTable(table,i+1,j-1);
+		table[i+1]=temp;
+		for(int k =i+2;k<=j; k++) {
+			table[k] = subtable[k-i-2];
+		}
+	}
+	
+
+	
+	
+	/**
+	 * @param table
+	 * @return
+	 * Returns the index of the smallest String of a table of String 
+	 */
 	private static int smaller(String[] table) {
-	// Returns the index of the smallest String of a table of String 
 		int index=0;
 		for(int i=1;i<table.length;i++) {
 			if(table[i].length() < table[index].length()) {
@@ -53,37 +92,62 @@ public class TextAnalyzer {
 		return index;
 	}
 	
+	/**
+	 * @param table
+	 * This method sorts the worlds of a given table of String by length
+	 */
 	private static void sortByLength(String[] table) {
 		int nb=table.length; 
+		int smaller;
 		for(int i=0;i<nb-1;i++) {
-			int smaller =smaller(subTable(table,i,nb-1));
-			if(smaller!=0) replace(table,i,i+smaller);
+			smaller =smaller(subTable(table,i,nb-1));
+			if(smaller>0) replace(table,i,i+smaller);// to avoid replacing anything when the word is well placed 
 		}
 	}
-	public static String[] tableSort(String[] tab) {
-	// This method sorts a given table of String objects. It will be used in the class.
+	
+	/**
+	 * @param tab
+	 * @return a table of String 
+	 * This method sorts a given table of String objects. The method uses the sort method given by JAVA API
+	 */
+	public static void sortByASCII(String[] tab) {
 		Arrays.sort(tab);
-		return tab;
 	}
 	
+	/**
+	 * @param table
+	 * @return
+	 * This method use the two method of sorting to provide the last sorted table needed.
+	 */
+	public static void sort(String[] table) {
+		sortByASCII(table);
+		sortByLength(table);
+		
+	}
+	/**
+	 * @return
+	 * This method counts how many different words we have on the text
+	 */
 	private int numberOfDistinctElements() {
-		// This method counts how many different words we have on the text
 		int total=0;
-		String[] sortedTab =tableSort(this.cut());
+		String[] sortedTab=this.cut();
+		sort(sortedTab);
 		int nb=sortedTab.length;
 		for(int index=0; index<nb-1; index++) {
 			if(!(sortedTab[index].equals(sortedTab[index+1]))) total++;
 		}
 		//For the last element of the table:
-		if(!(sortedTab[nb-1].equals(sortedTab[nb-2]))){
-			total++;
-		}
+		if(!(sortedTab[nb-1].equals(sortedTab[nb-2]))) total++;
 		return total;
 	}
 	
-	public String[] count(){
-		//Return the sorted table and the occurrence of each word
-		String[] sortedTab =tableSort(this.cut());
+	/**
+	 * @return
+	 * Return the sorted table and the occurrence of each word
+	 */
+	private String[] count(){
+		String[] sortedTab =this.cut();
+		sort(sortedTab);
 		String[] countingResult = new String[this.numberOfDistinctElements()];
 		int nb=sortedTab.length;
 		int count = 1;// The number of time a word appears 
@@ -104,7 +168,21 @@ public class TextAnalyzer {
 		return countingResult;
 	}
 	
+	/**
+	 * @param textToAnalyze
+	 * Prints the result of analyze of the given text 
+	 */
+	public void analyze() {
+		String [] countingResult = this.count();
+		for( int i=0; i<countingResult.length; i++) {
+			System.out.println(countingResult[i]);
+		}
+	}
+	
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		String text="The quick brown fox jumped over the lazy brown dog’s back";
 		TextAnalyzer textToAnalyze= new TextAnalyzer(text);
@@ -116,8 +194,8 @@ public class TextAnalyzer {
 		replace(table,1,4);
 		for( int i=0; i<table.length; i++) {
 			System.out.println(table[i]);
-		}
-		*/
+		}*/
+		
 		
 		/*//Test:smaller();
 		for(int i=0;i<table.length-1;i++) {
@@ -131,11 +209,14 @@ public class TextAnalyzer {
 			
 		}
 		*/
+		
+		/*
 		//Test:subtable;
 		String[] subtable=subTable(table,2,10);
+		*/
 		
 		//Test: sortByLength()
-		sortByLength(table);
+		sort(table);
 		for( int i=0; i<table.length; i++) {
 			System.out.println(table[i]);
 		}
